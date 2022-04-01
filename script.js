838b5ef4625ce5b7e3e5ae547d07141911ce2ae3
Man-Di <perfect8778@gmail.com>
2022-04-01 21:39:52 +0300

    1.04.2022

--- script.js
+++ script.js
@@ -2,8 +2,30 @@
 //     console.log(parametr);
 // }
 
-function f1(a) {
-    while( )
-    document.getElementById()
+var matrix = [
+[1,2,3],
+[4,5,6],
+[7,8,9],
+];
+
+function f1() {
+    // document.getElementById()
     console.log("privet Dimulik");
+}
+
+// console.log(matrix(0, 1));
+
+
+
+function f2(a) {
+    if (true) {
+        f1();
+    }
+        if (false) {
+        f3();   
+        }
+}
+
+function f3() {
+    console.log("сработала функция 3");
 }
